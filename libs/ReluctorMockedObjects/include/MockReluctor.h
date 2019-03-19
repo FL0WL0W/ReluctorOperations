62fb35891969d9201a9180d8e1ba464a38454e0f
Daniel Johnson <FL0WL0W@Cracked-Ice.com>
2019-03-18 22:27:59 -0500

    Update MockReluctor.h

--- libs/ReluctorMockedObjects/include/MockReluctor.h
+++ libs/ReluctorMockedObjects/include/MockReluctor.h
@@ -8,7 +8,7 @@ namespace Reluctor
 	{
 	public:
 		MOCK_METHOD0(GetPosition, float(void));
-		MOCK_METHOD0(GetTickPerDegree, uint32_t(void));
+		MOCK_METHOD0(GetTickPerDegree, float(void));
 		MOCK_METHOD0(GetRpm, uint16_t(void));
 		MOCK_METHOD0(GetResolution, uint16_t(void));
 		MOCK_METHOD0(IsSynced, bool(void));
