9e02adf0dc857f1e1dcf6c03411b5fe5b13f34c4
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2022-07-16 01:53:26 -0500

    selectable edges for Universal 1X reluctor

--- include/Operations/Operation_ReluctorUniversal1x.h
+++ include/Operations/Operation_ReluctorUniversal1x.h
@@ -7,13 +7,20 @@
 #define OPERATION_RELUCTORUNIVERSAL1X_H
 namespace ReluctorOperations
 {
+	enum Operation_ReluctorUniversal1xMode {
+		BothEdges = 0,
+		RisingEdge = 1,
+		FallingEdge = 2
+	};
+
 	class Operation_ReluctorUniversal1x : public OperationArchitecture::IOperation<ReluctorResult, EmbeddedIOOperations::Record<bool>*, EmbeddedIOServices::tick_t>
 	{
 	protected:
-		const position_t _risingPostion;
+		const position_t _risingPosition;
 		const position_t _fallingPosition;
+		const Operation_ReluctorUniversal1xMode _mode;
 	public:		
-        Operation_ReluctorUniversal1x(const position_t risingPostion, const position_t fallingPosition);
+        Operation_ReluctorUniversal1x(const Operation_ReluctorUniversal1xMode mode, const position_t risingPosition, const position_t fallingPosition);
 
 		ReluctorResult Execute(EmbeddedIOOperations::Record<bool> *, EmbeddedIOServices::tick_t) override;
 
--- src/Operations/Operation_ReluctorUniversal1x.cpp
+++ src/Operations/Operation_ReluctorUniversal1x.cpp
@@ -8,8 +8,9 @@ using namespace EmbeddedIOOperations;
 #ifdef OPERATION_RELUCTORUNIVERSAL1X_H
 namespace ReluctorOperations
 {
-	Operation_ReluctorUniversal1x::Operation_ReluctorUniversal1x(const position_t risingPostion, const position_t fallingPosition) :
-		_risingPostion(risingPostion),
+	Operation_ReluctorUniversal1x::Operation_ReluctorUniversal1x(const Operation_ReluctorUniversal1xMode mode, const position_t risingPosition, const position_t fallingPosition) :
+		_mode(mode),
+		_risingPosition(risingPosition),
 		_fallingPosition(fallingPosition)
 	{ }
 
@@ -31,9 +32,9 @@ namespace ReluctorOperations
 				return ret;
 		}
 
-		frameindex_t lastMinus1 =  Record<bool>::Subtract(last, 1, record->Length);
-		frameindex_t lastMinus2 =  Record<bool>::Subtract(last, 2, record->Length);
-		frameindex_t lastMinus4 =  Record<bool>::Subtract(last, 4, record->Length);
+		frameindex_t lastMinus1 = Record<bool>::Subtract(last, 1, record->Length);
+		frameindex_t lastMinus2 = Record<bool>::Subtract(last, 2, record->Length);
+		frameindex_t lastMinus4 = Record<bool>::Subtract(last, 4, record->Length);
 
 		if(!record->Frames[lastMinus2].Valid || !record->Frames[lastMinus4].Valid)
 			return ret;
@@ -47,27 +48,55 @@ namespace ReluctorOperations
 		if((delta2 * 2) < delta3 || (delta3 * 2) < delta2)
 			return ret;
 
-		position_t deltaPosition = 0;
-		position_t basePosition = 0;
-		if(record->Frames[last].State)
+		if(_mode == BothEdges)
 		{
-			deltaPosition = _risingPostion - _fallingPosition;
-			basePosition = _risingPostion;
+			position_t deltaPosition = 0;
+			position_t basePosition = 0;
+			if(record->Frames[last].State)
+			{
+				deltaPosition = _risingPosition - _fallingPosition;
+				basePosition = _risingPosition;
+			}
+			else
+			{
+				deltaPosition = _fallingPosition - _risingPosition;
+				basePosition = _fallingPosition;
+			}
+
+			while(deltaPosition < 0)
+				deltaPosition += 360;
+			//account for negative positions and weird positions > 360
+			while(deltaPosition > 360)
+				deltaPosition -= 360;
+
+			ret.PositionDot = deltaPosition / (record->Frames[last].Tick - record->Frames[lastMinus1].Tick);
+			ret.Position = basePosition + ((ret.CalculatedTick - record->Frames[last].Tick) * ret.PositionDot);
 		}
 		else
 		{
-			deltaPosition = _fallingPosition - _risingPostion;
-			basePosition = _fallingPosition;
+			tick_t lastTick = record->Frames[last].Tick;
+			if(_mode == RisingEdge)
+			{
+				if(!record->Frames[last].State)
+				{
+					last = lastMinus1;
+					lastMinus1 = Record<bool>::Subtract(last, 1, record->Length);
+				}
+				ret.Position = _risingPosition;
+			}
+			if(_mode == FallingEdge)
+			{
+				if(record->Frames[last].State)
+				{
+					last = lastMinus1;
+					lastMinus1 = Record<bool>::Subtract(last, 1, record->Length);
+				}
+				ret.Position = _fallingPosition;
+			}
+			ret.PositionDot = 360 / (record->Frames[last].Tick - record->Frames[lastMinus1].Tick);
+			ret.Position += (ret.CalculatedTick - record->Frames[last].Tick) * ret.PositionDot;
 		}
 
-		while(deltaPosition < 0)
-			deltaPosition += 360;
-		//account for negative positions and weird positions > 360
-		while(deltaPosition > 360)
-			deltaPosition -= 360;
-
-		ret.PositionDot = deltaPosition / (record->Frames[last].Tick - record->Frames[lastMinus1].Tick);
-		ret.Position = basePosition + ((ret.CalculatedTick - record->Frames[last].Tick) * ret.PositionDot);
 		while(ret.Position > 360)
 			ret.Position -= 360;
 		ret.PositionDot *= record->TicksPerSecond;
@@ -77,7 +106,28 @@ namespace ReluctorOperations
 
 	IOperationBase *Operation_ReluctorUniversal1x::Create(const void *config, size_t &sizeOut)
 	{
-		return new Operation_ReluctorUniversal1x(Config::CastAndOffset<position_t>(config, sizeOut), Config::CastAndOffset<position_t>(config, sizeOut));
+		const Operation_ReluctorUniversal1xMode mode = Config::CastAndOffset<Operation_ReluctorUniversal1xMode>(config, sizeOut);
+		
+		if(mode == BothEdges)
+		{
+			const position_t rising = Config::CastAndOffset<position_t>(config, sizeOut);
+			const position_t falling = Config::CastAndOffset<position_t>(config, sizeOut);
+			return new Operation_ReluctorUniversal1x(mode, rising, falling);
+		}
+		
+		if(mode == RisingEdge)
+		{
+			const position_t rising = Config::CastAndOffset<position_t>(config, sizeOut);
+			return new Operation_ReluctorUniversal1x(mode, rising, 0);
+		}
+		
+		if(mode == FallingEdge)
+		{
+			const position_t falling = Config::CastAndOffset<position_t>(config, sizeOut);
+			return new Operation_ReluctorUniversal1x(mode, 0, falling);
+		}
+
+		return 0;
 	}
 }
 #endif
